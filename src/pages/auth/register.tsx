c9f3d135c80d6c17a38c0a44394fbc6aba701dad
Isaac Quiroz Madrigal <isaac_817221mayorix@hotmail.com>
2022-02-02 08:38:21 -0500

    vista util de login y register

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from "./components";
+import FormLayout from "./layout/form-layout";
+
+
+export function Register() {
+
+  return (
+    <FormLayout type='register' >
+      <RegisterForm />
+    </FormLayout>
+  );
+}
